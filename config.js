3f46727d84438f324ddd4f751a020b5b0b14d91e
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-03-08 15:15:38 -0300

    The lb now has checks every request for a checksum match and has nice xml error responses when the checksum fails or the meeting was not created using the lb.

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 // NOTE: See config_local.js for local private configurations.
 var config = require('./config_local');
 
-config.bbb = config.bbb || {};
-config.bbb.api_path = '/bigbluebutton/api';
-
 config.nagios = config.nagios || {};
 config.nagios.url = 'http://143.54.12.174/nagios';
 config.nagios.api_path = '/cgi-bin/status-json.cgi';
@@ -11,6 +8,22 @@ config.nagios.auth = 'HTTPBasic'; // set to null for no auth
 config.nagios.interval = 20000; // in ms
 config.nagios.services = [ 'BigBlueButton Info', 'Network Report',
                            'Processor Report', 'Memory Report' ];
+
+config.bbb = config.bbb || {};
+config.bbb.api_path = '/bigbluebutton/api';
+config.bbb.responses = {}
+config.bbb.responses.checksumError =
+  '<response> \
+     <returncode>FAILED</returncode> \
+     <messageKey>checksumError</messageKey> \
+     <message>You did not pass the checksum security check</message> \
+   </response>';
+config.bbb.responses.invalidMeeting =
+  '<response> \
+     <returncode>FAILED</returncode> \
+     <messageKey>invalidMeetingIdentifier</messageKey> \
+     <message>The meeting ID that you supplied did not match any existing meetings</message> \
+   </response>';
 
 // for reference, in case we need process.env in the future:
 // config.twitter.user_name = process.env.TWITTER_USER || 'username';
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var url = require('url')
   , Nagios = require('../lib/nagios')
   , LoadBalancer = require('../lib/load_balancer')
   , Logger = require('../lib/logger')
+  , Utils = require('../lib/utils')
+  , sha1 = require('sha1')
   , config = require('../config');
 
 exports.index = function(req, res){
@@ -13,7 +15,41 @@ exports.api_index = function(req, res){
   res.render('index', { title: 'Mconf - BigBlueButton Load Balancer - Api Index' })
 };
 
+// Validates the checksum in the request 'req'.
+// If it doesn't match the expected checksum, we'll send
+// an XML response with an error code just like BBB does and
+// return false. Returns true if the checksum matches.
+exports.validateChecksum = function(req, res){
+  var method, query, salt, urlObj, checksum;
+
+  urlObj = url.parse(req.url, true);
+  checksum = urlObj.query['checksum'];
+  delete urlObj.search; // so the next line has effect
+  delete urlObj.query['checksum'];
+
+  // get the expected checksum
+  // note: the url query is already encoded, howerver BBB expects a ' ' to
+  // be encoded as '+', but any ' ' or '+' in the query are replaced by
+  // '%20' in the 'url.parse()' call above
+  query = Utils.bbbQueryFromUrl(urlObj).replace(/%20/g, '+');
+  method = Utils.bbbMethodFromUrl(urlObj);
+  salt = config.lb.salt;
+  correctChecksum = sha1(method + query + salt);
+
+  // matches the checksum
+  if (checksum != correctChecksum) {
+    Logger.log('checksum check failed, sending a checksumError response', m_id);
+    res.contentType('xml');
+    res.send(config.bbb.responses.checksumError);
+    return false;
+  }
+  return true;
+};
+
+// Routing a 'create' request
 exports.create = function(req, res){
+  if (!exports.validateChecksum(req, res)) return;
+
   urlObj = url.parse(req.url, true);
   var m_id = urlObj.query['meetingID'];
   Logger.log(urlObj.pathname + ' request with: ' + JSON.stringify(urlObj.query), m_id);
@@ -36,16 +72,20 @@ exports.create = function(req, res){
   });
 };
 
+// Routing any request that simply needs to be redirected to a BBB server
 exports.redirect = function(req, res){
+  if (!exports.validateChecksum(req, res)) return;
+
   urlObj = url.parse(req.url, true);
   var m_id = urlObj.query['meetingID'];
   Logger.log(urlObj.pathname + ' request with: ' + JSON.stringify(urlObj.query), m_id);
 
   Meeting.get(m_id, function(err, meeting){
     if (!meeting) {
-      Logger.log('failed to load meeting', m_id);
-      // TODO: better response, xml, maybe simulate a BBB default error response
-      res.send('This meeting was not created using this load balancer');
+      Logger.log('failed to load meeting, sending an invalidMeeting response', m_id);
+      res.contentType('xml');
+      res.send(config.bbb.responses.invalidMeeting);
+      return false;
     }
 
     LoadBalancer.redirect(req, res, meeting.server);
